c8c5c9b540b635428419adb311a9bc0f9132cc5a
Seif El-Din Sweilam <saifsweelam@gmail.com>
2024-06-01 05:55:47 +0300

    feat: Create auth service with  and

---
+++ auth/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            extends: [
+                "airbnb-base",
+                "airbnb-typescript/base",
+                "prettier",
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended",
+            ],
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                project: "tsconfig.json",
+                sourceType: "module",
+            },
+            rules:{
+                "linebreak-style": 0
+            }
+        },
+    ],
+};
