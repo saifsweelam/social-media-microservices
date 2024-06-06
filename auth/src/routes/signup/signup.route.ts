8a14efae84f150ef045d57fc2872bcbf39f61728
Seif El-Din Sweilam <saifsweelam@gmail.com>
2024-06-06 05:56:53 +0300

    feat(auth): Implement method validation for signup endpoint

--- auth/src/routes/signup/signup.route.ts
+++ auth/src/routes/signup/signup.route.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 
 import validator from './signup.validator';
 
+export const SIGN_UP_ROUTE = '/api/auth/signup';
+
 const signUpRouter = Router();
 
 signUpRouter.post('/', validator, (req, res) => {
     res.json({});
+});
+
+signUpRouter.all('/', (req, res) => {
+    res.status(405).json({ message: 'Method Not Allowed' });
 });
 
 export default signUpRouter;
--- auth/src/routes/signup/signup.test.ts
+++ auth/src/routes/signup/signup.test.ts
@@ -1,49 +1,69 @@
 import supertest from 'supertest';
 import app from '../../app';
+import { SIGN_UP_ROUTE } from './signup.route';
 
-describe('POST /api/auth/signup', () => {
+describe(`POST ${SIGN_UP_ROUTE}`, () => {
+    const validEmail = 'validEmail@mail.com';
+    const validPassword = 'validPassw0rd';
+
+    describe('Request Method Validation', () => {
+        it('responds with a 405 status code if the request method is not POST', async () => {
+            await supertest(app).get(SIGN_UP_ROUTE).expect(405);
+
+            await supertest(app).put(SIGN_UP_ROUTE).expect(405);
+
+            await supertest(app).patch(SIGN_UP_ROUTE).expect(405);
+
+            await supertest(app).delete(SIGN_UP_ROUTE).expect(405);
+        });
+
+        it('responds with a 200 status code if the request method is POST', async () => {
+            await supertest(app)
+                .post(SIGN_UP_ROUTE)
+                .send({ email: validEmail, password: validPassword })
+                .expect(200);
+        });
+    });
+
     describe('Email validation', () => {
-        const validPassword = 'validPassw0rd';
-
         it('responds with a 400 status code in case of invalid email', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ password: validPassword })
                 .expect(400);
+
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: 'invalid_email', password: validPassword })
                 .expect(400);
         });
 
         it('responds with a 200 status code in case of valid email', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: 'validEmail@mail.com', password: validPassword })
                 .expect(200);
         });
     });
 
     describe('Password validation', () => {
-        const validEmail = 'validEmail@mail.com';
-
         it('responds with a 400 status code in case of no password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: validEmail })
                 .expect(400);
         });
 
         it('responds with a 400 status code in case of too short password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: validEmail, password: 'Sh0rt' })
                 .expect(400);
         });
 
         it('responds with a 400 status code in case of too long password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({
                     email: validEmail,
                     password:
@@ -54,28 +74,28 @@ describe('POST /api/auth/signup', () => {
 
         it('responds with a 400 status code in case of no uppercase characters in password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: validEmail, password: 'longpassword1' })
                 .expect(400);
         });
 
         it('responds with a 400 status code in case of no lowercase characters in password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: validEmail, password: 'ALPHA2020' })
                 .expect(400);
         });
 
         it('responds with a 400 status code in case of no numbers in password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: validEmail, password: 'longPassword' })
                 .expect(400);
         });
 
         it('responds with a 200 status code in case of valid password', async () => {
             await supertest(app)
-                .post('/api/auth/signup')
+                .post(SIGN_UP_ROUTE)
                 .send({ email: validEmail, password: 'ValidPassw0rd' })
                 .expect(200);
         });
