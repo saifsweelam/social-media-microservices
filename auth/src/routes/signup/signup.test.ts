5b59c9df661351c76aa07edaa09150fc142066cb
Seif El-Din Sweilam <saifsweelam@gmail.com>
2024-06-06 05:32:03 +0300

    feat(auth): Add password validation to sign up

--- auth/src/routes/signup/signup.test.ts
+++ auth/src/routes/signup/signup.test.ts
@@ -2,11 +2,82 @@ import supertest from 'supertest';
 import app from '../../app';
 
 describe('POST /api/auth/signup', () => {
-    it('responds with a 400 status code in case of invalid email', async () => {
-        await supertest(app).post('/api/auth/signup').send({}).expect(400);
-        await supertest(app)
-            .post('/api/auth/signup')
-            .send({ email: 'invalid_email' })
-            .expect(400);
+    describe('Email validation', () => {
+        const validPassword = 'validPassw0rd';
+
+        it('responds with a 400 status code in case of invalid email', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ password: validPassword })
+                .expect(400);
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: 'invalid_email', password: validPassword })
+                .expect(400);
+        });
+
+        it('responds with a 200 status code in case of valid email', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: 'validEmail@mail.com', password: validPassword })
+                .expect(200);
+        });
+    });
+
+    describe('Password validation', () => {
+        const validEmail = 'validEmail@mail.com';
+
+        it('responds with a 400 status code in case of no password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: validEmail })
+                .expect(400);
+        });
+
+        it('responds with a 400 status code in case of too short password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: validEmail, password: 'Sh0rt' })
+                .expect(400);
+        });
+
+        it('responds with a 400 status code in case of too long password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({
+                    email: validEmail,
+                    password:
+                        'wayTooLongPasswordThatIWontBeEvenAbleToRemember22',
+                })
+                .expect(400);
+        });
+
+        it('responds with a 400 status code in case of no uppercase characters in password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: validEmail, password: 'longpassword1' })
+                .expect(400);
+        });
+
+        it('responds with a 400 status code in case of no lowercase characters in password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: validEmail, password: 'ALPHA2020' })
+                .expect(400);
+        });
+
+        it('responds with a 400 status code in case of no numbers in password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: validEmail, password: 'longPassword' })
+                .expect(400);
+        });
+
+        it('responds with a 200 status code in case of valid password', async () => {
+            await supertest(app)
+                .post('/api/auth/signup')
+                .send({ email: validEmail, password: 'ValidPassw0rd' })
+                .expect(200);
+        });
     });
 });
--- auth/src/routes/signup/signup.validator.ts
+++ auth/src/routes/signup/signup.validator.ts
@@ -4,12 +4,31 @@ import { validate } from 'zod-express-validator';
 const signUpValidator = validate(
     {
         body: z.object({
-            email: z.string().email(),
+            email: z.string().email({ message: 'Invalid email' }),
+            password: z
+                .string()
+                .min(8, {
+                    message: 'The password must be at least 8 characters',
+                })
+                .max(32, {
+                    message: 'The password must be at most 32 characters',
+                })
+                .regex(/[A-Z]/, {
+                    message:
+                        'The password must contain at least one uppercase character',
+                })
+                .regex(/[a-z]/, {
+                    message:
+                        'The password must contain at least one lowercase character',
+                })
+                .regex(/[0-9]/, {
+                    message: 'The password must contain at least one number',
+                }),
         }),
     },
     ({ bodyError, paramsError, queryError }, res) => {
         const error = bodyError ?? paramsError ?? queryError;
-        return res.status(400).json({ error: error?.message });
+        return res.status(400).json({ errors: error?.errors });
     },
 );
 
